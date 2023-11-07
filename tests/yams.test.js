e4d3438fe07468c47ed3cd3f0f4e28ee37153828
Choetsu <samy.amh94@gmail.com>
2023-11-07 14:32:54 +0100

    implémentation d'un test non valide pour la vérification de l'obtention d'un yams

--- tests/yams.test.js
+++ tests/yams.test.js
@@ -4,4 +4,8 @@ describe("Yams", () => {
     it("should return 50 points if all dice are the same (YAMS)", () => {
         expect(calculatePoints([4, 4, 4, 4, 4])).toBe(50);
     });
+
+    it("should return an error if is not YAMS", () => {
+        expect(calculatePoints([4, 4, 4, 4, 3])).toBe(50);
+    });
 });
