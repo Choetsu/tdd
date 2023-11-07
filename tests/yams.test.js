5df4ef62afcac4b1e7e88947f7d2aadd25dc061a
Choetsu <samy.amh94@gmail.com>
2023-11-07 14:37:44 +0100

    implémentation d'un test non valide pour la vérification de l'obtention d'un carré

--- tests/yams.test.js
+++ tests/yams.test.js
@@ -4,11 +4,14 @@ describe("Yams", () => {
     it("should return 50 points if all dice are the same (YAMS)", () => {
         expect(calculatePoints([4, 4, 4, 4, 4])).toBe(50);
     });
-    it("should return an error if is not YAMS", () => {
+    it("should not return 50 points if all dice are not the same (YAMS)", () => {
         expect(calculatePoints([4, 4, 4, 4, 3])).not.toBe(50);
     });
 
     it("should return 35 points if 4 dice are the same (Carré)", () => {
         expect(calculatePoints([4, 4, 4, 4, 3])).toBe(35);
+    });
+    it("should not return 35 points if 4 dice are not the same (Carré)", () => {
+        expect(calculatePoints([4, 4, 4, 3, 3])).not.toBe(35);
     });
 });
